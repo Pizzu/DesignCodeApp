28d752eccac4697d14c0598ef0c08e54ccde2e65
Pizzu <lucaloforte3@gmail.com>
2021-03-01 14:29:52 +0100

    Fix UI when the keyboard appears.

--- DesignCode/Views/AuthView.swift
+++ DesignCode/Views/AuthView.swift
@@ -26,9 +26,8 @@ struct AuthView: View {
             
             Color.black.ignoresSafeArea()
             
-            ZStack(alignment: .top) {
+            ZStack {
                 
-
                 Color("background2")
                     .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
                     .edgesIgnoringSafeArea(.bottom)
@@ -82,32 +81,37 @@ struct AuthView: View {
                 .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
                 .shadow(color: Color.black.opacity(0.15), radius: 20, x: 0, y: 20)
                 .padding(.horizontal)
-                .offset(y: 460)
+                .offset(y: 160)
                 
-                HStack {
-                    Text("Forgot Password?")
-                        .font(.subheadline)
+                VStack {
                     
                     Spacer()
                     
-                    Button(action: {
-                        self.showAlert = true
-                        self.isFocus = false
-                        self.hideKeyboard()
-                    }) {
-                        Text("Log in").foregroundColor(.black)
+                    HStack {
+                        Text("Forgot Password?")
+                            .font(.subheadline)
+                        
+                        Spacer()
+                        
+                        Button(action: {
+                            self.showAlert = true
+                            self.isFocus = false
+                            self.hideKeyboard()
+                        }) {
+                            Text("Log in").foregroundColor(.black)
+                        }
+                        .padding(12)
+                        .padding(.horizontal, 30)
+                        .background(Color(#colorLiteral(red: 0, green: 0.7529411765, blue: 1, alpha: 1)))
+                        .clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous))
+                        .shadow(color: Color(#colorLiteral(red: 0, green: 0.7529411765, blue: 1, alpha: 1)).opacity(0.3), radius: 20, x: 0.0, y: 20)
+                        .alert(isPresented: $showAlert, content: {
+                            Alert(title: Text("Error"), message: Text(self.alertMessage), dismissButton: .default(Text("Ok")))
+                        })
                     }
-                    .padding(12)
-                    .padding(.horizontal, 30)
-                    .background(Color(#colorLiteral(red: 0, green: 0.7529411765, blue: 1, alpha: 1)))
-                    .clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous))
-                    .shadow(color: Color(#colorLiteral(red: 0, green: 0.7529411765, blue: 1, alpha: 1)).opacity(0.3), radius: 20, x: 0.0, y: 20)
-                    .alert(isPresented: $showAlert, content: {
-                        Alert(title: Text("Error"), message: Text(self.alertMessage), dismissButton: .default(Text("Ok")))
-                    })
+                    .frame(maxWidth: .infinity)
+                    .padding()
                 }
-                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottom)
-                .padding()
                 
             }
             .offset(y: self.isFocus ? -300 : 0)
@@ -116,6 +120,7 @@ struct AuthView: View {
                 self.isFocus = false
                 self.hideKeyboard()
             }
+            .ignoresSafeArea(.keyboard, edges: .bottom)
         }
     }
 }
@@ -133,67 +138,68 @@ struct CoverView: View {
     @State private var dragPosition : CGSize = CGSize.zero
     
     var body: some View {
-        VStack(spacing: 15.0) {
-            Text("Learn design & code.\nFrom scratch.")
-                .font(.largeTitle, weight: .bold)
-                .foregroundColor(Color.white)
-                .multilineTextAlignment(.center)
-                .offset(x: self.dragPosition.width / 15, y: self.dragPosition.height / 15)
-            
-            Text("80 hours of courses of SwiftUI, React and design tools.")
-                .font(.subheadline)
-                .frame(width: 250)
-                .multilineTextAlignment(.center)
-                .offset(x: self.dragPosition.width / 20, y: self.dragPosition.height / 20)
+        VStack {
+            VStack(spacing: 15.0) {
+                Text("Learn design & code.\nFrom scratch.")
+                    .font(.largeTitle, weight: .bold)
+                    .foregroundColor(Color.white)
+                    .multilineTextAlignment(.center)
+                    .offset(x: self.dragPosition.width / 15, y: self.dragPosition.height / 15)
+                
+                Text("80 hours of courses of SwiftUI, React and design tools.")
+                    .font(.subheadline)
+                    .frame(width: 250)
+                    .multilineTextAlignment(.center)
+                    .offset(x: self.dragPosition.width / 20, y: self.dragPosition.height / 20)
+                
+                Spacer()
+            }
+            .padding(.top, 100)
+            .frame(height: 477)
+            .frame(maxWidth: .infinity)
+            .background(
+                Image(uiImage: #imageLiteral(resourceName: "Card3"))
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(maxWidth: 500)
+                    .offset(x: self.dragPosition.width / 25, y: self.dragPosition.height / 25)
+                ,
+                alignment: .bottom
+            )
+            .background(
+                Image(uiImage: #imageLiteral(resourceName: "Blob"))
+                    .offset(x: -150, y: -200)
+                    .rotationEffect(Angle(degrees: self.show ? 360+90 : 90))
+                    .blendMode(.plusDarker)
+                    .animation(Animation.linear(duration: 120).repeatForever(autoreverses: false), value: self.show)
+            )
+            .background(
+                Image(uiImage: #imageLiteral(resourceName: "Blob"))
+                    .offset(x: -200, y: -250)
+                    .rotationEffect(Angle(degrees: self.show ? 360 : 0), anchor:.center)
+                    .blendMode(.overlay)
+                    .animation(Animation.linear(duration: 100).repeatForever(autoreverses: false), value: self.show)
+            )
+            .background(Color(#colorLiteral(red: 0.4117647059, green: 0.4705882353, blue: 1, alpha: 1)))
+            .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
+            .scaleEffect(self.isDragging ? 0.92 : 1)
+            .animation(.timingCurve(0.2, 0.8, 0.2, 1, duration: 0.8))
+            .rotation3DEffect(
+                Angle(degrees: 5),
+                axis: (x: self.dragPosition.width, y: self.dragPosition.height, z: 0)
+            )
+            .gesture(
+                DragGesture().onChanged { value in
+                    self.dragPosition = value.translation
+                    self.isDragging = true
+                }
+                .onEnded { value in
+                    self.dragPosition = .zero
+                    self.isDragging = false
+                }
+            )
             
             Spacer()
         }
-        .padding(.top, 100)
-        .frame(height: 477)
-        .frame(maxWidth: .infinity)
-        .background(
-            Image(uiImage: #imageLiteral(resourceName: "Card3"))
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(maxWidth: 500)
-                .offset(x: self.dragPosition.width / 25, y: self.dragPosition.height / 25)
-            ,
-            alignment: .bottom
-        )
-        .background(
-            Image(uiImage: #imageLiteral(resourceName: "Blob"))
-                .offset(x: -150, y: -200)
-                .rotationEffect(Angle(degrees: self.show ? 360+90 : 90))
-                .blendMode(.plusDarker)
-                .animation(Animation.linear(duration: 120).repeatForever(autoreverses: false), value: self.show)
-        )
-        .background(
-            Image(uiImage: #imageLiteral(resourceName: "Blob"))
-                .offset(x: -200, y: -250)
-                .rotationEffect(Angle(degrees: self.show ? 360 : 0), anchor:.center)
-                .blendMode(.overlay)
-                .animation(Animation.linear(duration: 100).repeatForever(autoreverses: false), value: self.show)
-        )
-        .background(Color(#colorLiteral(red: 0.4117647059, green: 0.4705882353, blue: 1, alpha: 1)))
-        .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
-        .scaleEffect(self.isDragging ? 0.92 : 1)
-        .animation(.timingCurve(0.2, 0.8, 0.2, 1, duration: 0.8))
-        .rotation3DEffect(
-            Angle(degrees: 5),
-            axis: (x: self.dragPosition.width, y: self.dragPosition.height, z: 0)
-        )
-        .gesture(
-            DragGesture().onChanged { value in
-                self.dragPosition = value.translation
-                self.isDragging = true
-            }
-            .onEnded { value in
-                self.dragPosition = .zero
-                self.isDragging = false
-            }
-        )
-//        .onAppear {
-//            self.show = true
-//        }
     }
 }
